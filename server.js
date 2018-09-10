f5944262741ac81fc6cbd027ecf9fab4dd6f3f90
Predrag Vlajkovic <predrag.vlajkovic@gmail.com>
2018-09-11 00:48:53 +0200

    Improved voting system

--- server.js
+++ server.js
@@ -2,17 +2,11 @@ var express = require ('express');
 var app = express();
 
 var server = require('http').createServer(app);
-//var http = require('http');
-//var server = http.createServer(function(request, response){
-//  response.writeHead(200, {'Content-Type': 'text/html'});
-//  response.write('<h1>The server is working!</h1>');
-//  response.end();
-//});
+
 var io = require ('socket.io').listen (server);
 
-sessions = [];  // session, admin user
-users = [];	// user, session
-votes = []; // session, user, vote
+sessions = [];  // id(session), user
+votes = []; 	// session, user, vote
 connections = [];
 
 
@@ -24,47 +18,85 @@ app.get ('/', function (req, res) {
 	res.sendFile (__dirname + '/index.html');
 });
 
+// setup listener
 io.sockets.on('connection', function(socket) {
+
 	connections.push (socket);
 	console.log ('Client connected: %s sockets connected', connections.length);
-
+	
 	// Disconnect socket
 	socket.on ('disconnect', function (data) {
 		connections.splice (connections.indexOf(socket),1);
-		console.log ('Client disconnected: %s sockets connected', connections.length);		
+		console.log ('Client disconnected: %s sockets connected', connections.length);
 	});
 
 	// Session started
-	socket.on ('session started', function (data) {
-		sessions.push ( {id: data.id, user_id: data.user_id } );
-		users.push ( {id: data.user_id, session: data.id } );
-		console.log (sessions);
-		console.log (users);
+	socket.on ('session_started', function (data) {
+		console.log ("Session " + data.id + " started, user " + data.user + " is admin/scrum master");
+		sessions.push ( { id: data.id, user: data.user } );
+		votes.push ( { session: data.id, user: data.user, vote: null } );
+		socket.send ('session_started', data);		
 	});
 
-	// Session started
-	socket.on ('session joined', function (data) {
-		sessions.push ( {id: data.id, user_id: data.user_id } );
-		users.push ( {id: data.user_id, session: data.id } );
-		console.log (sessions);
-		console.log (users);
+	// Session joined
+	socket.on ('session_joined', function (data) {
+		console.log ("User '" + data.user + "' joined session " + data.session);
+		votes.push ( { session: data.session, user: data.user, vote: null } );
+		socket.send ('session_joined', votes);
 	});
 
-	// Session started
+	// Session left
+	socket.on ('session_left', function (data) {
+ 		for (var j=0; j<votes.length; j++) {
+    		if (votes[j].user === data.user && votes[j].session === data.session) {
+    			votes.splice (j,1);			    	
+    		}
+		}
+		console.log ("User '" + data.user + "' left session " + data.session);
+	});
+
+	// User has been voted
 	socket.on ('voted', function (data) {
 		var change = false;
-		for (var i = 0; i < votes.length; i++) {
+		var voted = [];
+		for (var i=0; i<votes.length; i++) {
 		    if (votes[i].session==data.session && votes[i].user==data.user ) {
 		    	votes[i].vote = data.vote;
 		    	change = true;
 		    }
 		}
 		if (! change) {
-			votes.push ( {session: data.session, user: data.user, vote: data.vote } );
+			votes.push ( { session: data.session, user: data.user, vote: data.vote } );
 		}
+		for (i=0; i<votes.length; i++) {
+		    if (votes[i].session==data.session) {
+				voted.push ( { session: votes[i].session, user: votes[i].user, vote: votes[i].vote } );
+		    }
+		}
+		io.sockets.emit ('votes', voted);
+		console.log ("Voted");
+		console.log (voted);
+		console.log ("Votes");
 		console.log (votes);
+
 	});	
+
+	// User has been voted
+	socket.on ('clear_votes', function (data) {
+		var voted = [];
+		for (var i=0; i<votes.length; i++) {
+		    if (votes[i].session==data.session ) {
+		    	votes[i].vote = null;
+		    }
+		}
+		for (i=0; i<votes.length; i++) {
+		    if (votes[i].session==data.session) {
+				voted.push ( { session: votes[i].session, user: votes[i].user, vote: votes[i].vote } );
+		    }
+		}
+		io.sockets.emit ('votes', voted);
+		console.log ("Voted");
+		console.log (voted);
+	});	
+
 });
-
-
-
