b2eccf3f7e3701d4217455860a11be1d942e9c63
Predrag Vlajkovic drPreAG <predrag.vlajkovic@gmail.com>
2018-10-29 22:19:59 +0100

    Added modules, new API endpoints

---
+++ modules/listeners.js
@@ -0,0 +1,131 @@
+		
+function disconnect (socket, users, votes, connection, data) {
+	console.log ('User %s on a session %d disconnected', users.indexOf(socket.id).username, users.indexOf(socket.id).session);
+	// delete votes
+	for (var i=0; i<votes.length; i++) {
+		if (votes[i].username == users.indexOf(socket.id).username && votes[i].session == users.indexOf(socket.id).session) {
+			user_exists = true;
+		}
+	}		
+	users.splice (users.indexOf(socket.id),1);		
+	connections.splice (connections.indexOf(socket),1);
+
+	socket.leave (data.session);
+	console.log ('Client disconnected: %s sockets connected', connections.length);	
+}
+
+function sessionJoined (socket, users, votes, data) {
+	// User joined to a session
+	socket.broadcast.emit ('session_joined', data);
+	users.push ( { socket: socket.id, session: data.session, username: data.username } );
+
+	var user_exist = false;
+	for (var i=0; i<votes.length; i++) {
+		if (votes[i].username == data.username && votes[i].session == data.session) {
+			user_exist = true;
+			console.log ('User %s re-joined session %s', data.username, data.session);    			
+		}
+	}			
+	if (!user_exist) {
+		votes.push ( { session: data.session, username: data.username, vote: null } );
+		socket.join (data.session);
+		// broadcast to all other clients in a channel
+		console.log ('User %s joined session %s', data.username, data.session);
+	}
+}
+
+function sessionReJoined (socket, users, votes, data) {
+	socket.broadcast.emit ('session_rejoined', data);
+	users.push ( { socket: socket.id, session: data.session, username: data.username } );
+
+	var user_exist = false;
+	for (var i=0; i<votes.length; i++) {
+		if (votes[i].username == data.username && votes[i].session == data.session) {
+			user_exists = true;
+		}
+	}			
+	if (!user_exist)
+		votes.push ( { session: data.session, username: data.username, vote: null } );		
+
+	socket.join (data.session);
+	// broadcast to all other clients in a channel
+	console.log ('User %s re-joined session %s', data.username, data.session);
+}
+
+function sessionLeft (socket, users, votes, data) {
+	socket.broadcast.emit ('session_left', data);
+		for (var i=0; i<votes.length; i++) {
+		if (votes[i].username == data.username && votes[i].session == data.session) {
+			votes.splice (i,1);
+		}
+	}	
+	users.splice (users.indexOf(socket.id),1);
+	socket.leave (data.session);
+	// broadcast to all other clients in a channel
+	console.log ('User %s left session %s', data.username, data.session);
+}
+
+function userVoted (socket, users, votes, data) {
+	socket.broadcast.emit ('voted', data);		
+	var change = false;
+	// add that users vote to votes[] (alter or push)
+	for (var i=0; i<votes.length; i++) {
+	    if (votes[i].session==data.session && votes[i].username==data.username ) {
+	    	votes[i].vote = data.vote;
+	    	change = true;
+	    }
+	}
+	if (! change) 
+		votes.push ( { session: Number(data.session), username: data.username, vote: data.vote } );
+	
+	var missing = false;
+	var voted = []; // new array with current session only
+	for (i=0; i<votes.length; i++) {
+	    if (votes[i].session == data.session) {
+			voted.push ( { session: Number(votes[i].session), username: votes[i].username, vote: votes[i].vote } );
+	    }
+	}	
+	console.log ('User %s voted in session %s', data.username, data.session);	
+}
+
+function clearVotes (socket, users, votes, data) {
+	// Votes have been cleared
+	for (var i=0; i<votes.length; i++) {
+	    if (votes[i].session == data.session ) {
+	    	votes[i].vote = null;
+	    }
+	}
+	socket.broadcast.emit ('clear_votes', data);
+	console.log ("In Session %d cleared votes by %s", data.session, data.username);
+}
+
+function changedSession (socket, session, data) {
+	// Someone changed vote algorythm
+	for (var i=0; i<sessions.length; i++) {
+	    if (sessions[i].session == data.session ) {
+	    	sessions[i].algorythm = data.algorythm;
+	    	sessions[i].story = data.story;
+			socket.broadcast.emit ('change_session', data);
+			console.log ("Session %d changed by %s", data.session, data.username);
+	    }
+	}		
+}
+
+function showAllVotes (socket, data) {
+	// lets show all votes	
+	for (var i=0; i<sessions.length; i++) {
+	    if (sessions[i].session == data.session ) {
+			socket.broadcast.emit ('show_all_votes', data);
+			console.log ("In Session %d user %s sent show all votes signal", data.session, data.username);
+		}
+	}
+}
+
+module.exports.disconnect = disconnect;
+module.exports.sessionJoined = sessionJoined;
+module.exports.sessionReJoined = sessionReJoined;
+module.exports.sessionLeft= sessionLeft;
+module.exports.userVoted = userVoted;
+module.exports.clearVotes = clearVotes;
+module.exports.changedSession = changedSession;
+module.exports.showAllVotes = showAllVotes;
--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+/*
+ * Node JS Express common part
+*/
 const express = require ('express');
 const app = express();
 app.use (express.json());
@@ -15,12 +18,21 @@ app.use (compression());
 const helmet = require('helmet');
 app.use (helmet());
 
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.disable('etag');
 
+/*
+ * 
+*/
+
+const listeners = require ('./modules/listeners.js');
+const common = require ('./modules/common.js');
 const serverPort = process.env.PORT || 3000;
 
-sessions = [ 		// session, user, algorythm, date
-	{ session: 91969, user: "Peca", algorythm: 2, date: "2018 Oct 01" }
+sessions = [ 		// session, username, algorythm, story, date
+	{ session: 91969, username: "Peca", algorythm: 2, story: '', date: Date.now() }
 ];  	
 users = [];			// socketid, session, username
 votes = []; 		// session, user, vote
@@ -30,16 +42,15 @@ server.listen (serverPort);
 
 console.log (`Poker plan server is up and running on port ${serverPort}...`);
 
-
-// index.html
+// GET index.html
 app.get ('/', function (req, res) {
 	console.log ('Request for /');	
 	res.sendFile (__dirname + '/index.html');
 });
 
-// api/sessions
+// GET /api/sessions
 app.get ('/api/sessions', function (req, res) {
-	console.log ("API request for /api/sessions");	
+	console.log ("API get request for /api/sessions");	
 
 	if (sessions.length===0) 
 		return res.status(404).send("Sessions not found");
@@ -47,9 +58,9 @@ app.get ('/api/sessions', function (req, res) {
 	console.log ("  Sessions found");
 });
 
-// api/session/:id
+// GET /api/sessions/:id
 app.get ('/api/sessions/:id', function (req, res) {
-	console.log ("API request for /api/sessions/" + req.params.id);
+	console.log ("API get request for /api/sessions/" + req.params.id);
 
 	if (isNaN(req.params.id)) 
 		return res.status(400).send("Invalid input " + req.params.id);
@@ -58,9 +69,48 @@ app.get ('/api/sessions/:id', function (req, res) {
 
 	if (session)
 		return res.send ({ session: sessionId, exists: true, algorythm: session.algorythm, story: session.story, date: session.date });
-	res.status(404).send("Session not found " +  sessionId);
+	res.status(404).send("Session not found " + sessionId);
 });
 
+// POST /api/sessions
+app.post ('/api/sessions', function (req, res) {
+	console.log ("API post request for /api/sessions/");
+
+    var session = null;
+    var exists = true;	
+
+    while (exists) {
+    	exists = false;
+		newSession = common.randomNumber(10000,99999)
+		for (var i=0; i<sessions.length; i++)
+			if (sessions[i].session == newSession) {
+				exists = true;
+				continue;
+			}
+	}
+	sessions.push ( { session: newSession, username: req.body.username, algorythm: 2, story: null, date: Date.now() } );
+
+	return res.send (sessions[sessions.length-1]);
+});
+
+// PUT /api/sessions/:id
+app.put ('/api/sessions/:id', function (req, res) {
+	console.log ("API put request for /api/sessions/" + req.params.id);
+
+	if (isNaN(req.params.id)) 
+		return res.status(400).send("Invalid input " + req.params.id);
+
+	const sessionId = parseInt(req.params.id);
+	const session = sessions.find (s => s.session === sessionId);
+	if (session) {
+		session.algorythm = req.body.algorythm;
+		session.story = req.body.story;
+		return res.send (session);
+	}
+	return res.status(500).send("Session error " + sessionId);
+});
+
+// GET /api/sessions/:id/votes
 app.get ('/api/sessions/:id/votes', function (req, res) {
 	console.log ('API request for /api/sessions/%d/votes/', req.params.id);	
 
@@ -93,143 +143,41 @@ io.on('connection', function(socket) {
 	
 	// Disconnected user (on a network level, not when user leave room)
 	socket.on ('disconnect', function (data) {
-		console.log ('User %s on a session %d disconnected', users.indexOf(socket.id).username, users.indexOf(socket.id).session);		
-
-		// delete votes
- 		for (var i=0; i<votes.length; i++) {
-    		if (votes[i].username == users.indexOf(socket.id).username && votes[i].session == users.indexOf(socket.id).session) {
-    			user_exists = true;
-    		}
-		}		
-		users.splice (users.indexOf(socket.id),1);		
-		connections.splice (connections.indexOf(socket),1);
-
-		socket.leave (data.session);
-		console.log ('Client disconnected: %s sockets connected', connections.length);
-	});
-
-	// Session started
-	socket.on ('session_started', function (data) {
-		sessions.push ( {session: data.session, username: data.username, algorythm: 1, story: null, date: Date.now()} );
-		console.log ("Session %s started by user %s @ %s", data.session, data.username, Date.now());		
+		listeners.disconnect (socket, users, votes, connections, data);
 	});
 
 	// User joined to a session
 	socket.on ('session_joined', function (data) {
-		socket.broadcast.emit ('session_joined', data);
-		users.push ( { socket: socket.id, session: data.session, username: data.username } );
-
-		var user_exist = false;
- 		for (var i=0; i<votes.length; i++) {
-    		if (votes[i].username == data.username && votes[i].session == data.session) {
-    			user_exist = true;
-				console.log ('User %s re-joined session %s', data.username, data.session);    			
-    		}
-		}			
-		if (!user_exist) {
-			votes.push ( { session: data.session, username: data.username, vote: null } );
-			socket.join (data.session);
-			// broadcast to all other clients in a channel
-			console.log ('User %s joined session %s', data.username, data.session);
-		}
+		listeners.sessionJoined (socket, users, votes, data);
 	});	
 
 	// User re-joined to a session
 	socket.on ('session_rejoined', function (data) {
-		socket.broadcast.emit ('session_rejoined', data);
-		users.push ( { socket: socket.id, session: data.session, username: data.username } );
-
-		var user_exist = false;
- 		for (var i=0; i<votes.length; i++) {
-    		if (votes[i].username == data.username && votes[i].session == data.session) {
-    			user_exists = true;
-    		}
-		}			
-		if (!user_exist)
-			votes.push ( { session: data.session, username: data.username, vote: null } );		
-
-		socket.join (data.session);
-		// broadcast to all other clients in a channel
-		console.log ('User %s re-joined session %s', data.username, data.session);
+		listeners.sessionReJoined (socket, users, votes, data);
 	});		
 
 	// User left a session
 	socket.on ('session_left', function (data) {
-		socket.broadcast.emit ('session_left', data);
-		console.log ('User %s left session %s', data.username, data.session);
- 		for (var i=0; i<votes.length; i++) {
-    		if (votes[i].username == data.username && votes[i].session == data.session) {
-    			votes.splice (i,1);
-    		}
-		}	
-		users.splice (users.indexOf(socket.id),1);
-		socket.leave (data.session);
-		// broadcast to all other clients in a channel
+		listeners.sessionLeft (socket, users, votes, data);
 	});	
 
 	// If one user has been voted, then send all votes to all users in a channel
 	socket.on ('voted', function (data) {
-		socket.broadcast.emit ('voted', data);		
-		var change = false;
-		// add that users vote to votes[] (alter or push)
-		for (var i=0; i<votes.length; i++) {
-		    if (votes[i].session==data.session && votes[i].username==data.username ) {
-		    	votes[i].vote = data.vote;
-		    	change = true;
-		    }
-		}
-		if (! change) {
-			votes.push ( { session: Number(data.session), username: data.username, vote: data.vote } );
-		}
-		var missing = false;
-		var voted = []; // new array with current session only
-		for (i=0; i<votes.length; i++) {
-		    if (votes[i].session == data.session) {
-				voted.push ( { session: Number(votes[i].session), username: votes[i].username, vote: votes[i].vote } );
-		    }
-		}		
+		listeners.userVoted (socket, users, votes, data);
 	});	
 
 	// Votes have been cleared
 	socket.on ('clear_votes', function (data) {
-		for (var i=0; i<votes.length; i++) {
-		    if (votes[i].session == data.session ) {
-		    	votes[i].vote = null;
-		    }
-		}
-		socket.broadcast.emit ('clear_votes', data);
-		console.log ("In Session %d cleared votes by %s", data.session, data.username);
+		listeners.clearVotes (socket, users, votes, data);
 	});	
 
 	// Someone changed vote algorythm
-	socket.on ('change_algorythm', function (data) {
-		for (var i=0; i<sessions.length; i++) {
-		    if (sessions[i].session == data.session ) {
-		    	sessions[i].algorythm = data.algorythm;
-				socket.broadcast.emit ('change_algorythm', data);
-				console.log ("In Session %d changed algorythm to %d by %s", data.session, data.algorythm, data.username);		    	
-		    }
-		}		
-	});		
-
-	// Someone changed story name
-	socket.on ('change_story', function (data) {
-		for (var i=0; i<sessions.length; i++) {
-		    if (sessions[i].session == data.session ) {
-		    	sessions[i].story = data.story;
-				socket.broadcast.emit ('change_story', data);
-				console.log ("In Session %d user %s changed story name to %s", data.session, data.username, data.story);		    	
-		    }
-		}		
+	socket.on ('changed_session', function (data) {
+		listeners.changedSession (socket, data);		
 	});		
 
 	// lets show all votes
 	socket.on ('show_all_votes', function (data) {
-		for (var i=0; i<sessions.length; i++) {
-		    if (sessions[i].session == data.session ) {
-				socket.broadcast.emit ('show_all_votes', data);
-				console.log ("In Session %d user %s sent show all votes signal", data.session, data.username);
-			}
-		}
+		listeners.showAllVotes (socket, data)	
 	});	
 });
